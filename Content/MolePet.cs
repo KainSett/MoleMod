35a9a013c82ba013862dfacb56e3dfeed8683d79
KainSett <7talessett@gmail.com>
2025-02-16 05:09:53 -0600

    Add files via upload

--- Content/MolePet.cs
+++ Content/MolePet.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -22,8 +23,9 @@ namespace MoleMod.Content
             Projectile.height = 46;
             Projectile.damage = 0;
             Projectile.ignoreWater = true;
-            Projectile.tileCollide = false;
             Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.velocity = new Vector2(0, 0);
         }
         public enum Animation
         {
@@ -44,12 +46,15 @@ namespace MoleMod.Content
         }
         public override void AI()
         {
-            Projectile.timeLeft++;
 
             var owner = Main.player[Projectile.owner];
 
-            if (owner == null || !owner.active || !owner.HasBuff(ModContent.BuffType<MolePetBuff>()))
+            if ((owner == null || !owner.active || !owner.HasBuff(ModContent.BuffType<MolePetBuff>()))) { 
                 Projectile.Kill();
+                return;
+            }
+
+            Projectile.timeLeft++;
 
 
             Projectile.frameCounter++;
@@ -79,16 +84,16 @@ namespace MoleMod.Content
                             Projectile.spriteDirection = 1;
 
                         else if (Projectile.frame == 3)
-                            Projectile.spriteDirection = -1;
+                            Projectile.spriteDirection = 0;
 
 
-                        if (Projectile.spriteDirection != 1) 
+                        if (Projectile.spriteDirection != 1)
                             Projectile.frame++;
-                            
-                        
+
+
 
                         else Projectile.frame--;
-                        
+
 
                         break;
 
@@ -96,25 +101,23 @@ namespace MoleMod.Content
                 }
             }
 
-            var top = Projectile.Center - new Vector2(0, Projectile.height / 2);
-            var bottom = Projectile.Center + new Vector2(0, Projectile.height / 1.8f);
+            var top = Projectile.Center + new Vector2(0, Projectile.height / 4);
+            var bottom = Projectile.Center + new Vector2(0, Projectile.height / 2);
 
-            if (WorldGen.SolidOrSlopedTile(top.ToTileCoordinates().X, top.ToTileCoordinates().Y) || !Main.tile[bottom.ToTileCoordinates()].HasTile)
-                CurrentAnimation = Animation.Burrowing;
-
-            
-
-            else if (Projectile.Center.DistanceSQ(owner.Center) < 300 * 300 && CurrentAnimation == Animation.Burrowing)
+            if (Projectile.Center.DistanceSQ(owner.Center) < 300 * 300 && CurrentAnimation == Animation.Burrowing)
                 CurrentAnimation = Animation.UnHiding;
 
             else if (CurrentAnimation == Animation.Stationary && Projectile.Center.DistanceSQ(owner.Center) > 300 * 300)
                 CurrentAnimation = Animation.Hiding;
 
+            if (WorldGen.SolidOrSlopedTile(top.ToTileCoordinates().X, top.ToTileCoordinates().Y) || !Main.tile[bottom.ToTileCoordinates()].HasTile)
+                CurrentAnimation = Animation.Burrowing;
+
             switch (CurrentAnimation)
             {
                 case Animation.Burrowing:
                     var rot = Projectile.Center.X > owner.Center.X ? -MathHelper.PiOver4 : MathHelper.PiOver4;
-                    Projectile.velocity = Projectile.Center.DirectionTo(owner.Center).RotatedBy(rot) * 5;
+                    Projectile.velocity = Projectile.Center.DirectionTo(owner.Center).RotatedBy(rot).SafeNormalize(Vector2.Zero) * 5;
                     Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
                     break;
 
@@ -123,9 +126,11 @@ namespace MoleMod.Content
                     Projectile.rotation = 0;
                     break;
             }
+
         }
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
+            Projectile.velocity = oldVelocity;
             if (oldVelocity.LengthSquared() > 1 && CurrentAnimation == Animation.Burrowing)
             {
                 for (int i = 4; i > 0; i--)
@@ -135,13 +140,25 @@ namespace MoleMod.Content
             }
             return false;
         }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            var texture = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value; 
+            int frameHeight = texture.Height / Main.projFrames[Type];
+            int startY = frameHeight * Projectile.frame;
+            var rect = new Rectangle(0, startY, texture.Width, frameHeight);
+            var scale = Projectile.scale * Main.GameZoomTarget;
+            var color = lightColor;
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, rect, color, Projectile.rotation, rect.Size() / 2, scale, (SpriteEffects)Projectile.spriteDirection, 0);
+
+            return false;
+        }
         public override void PostDraw(Color lightColor)
         {
             var texture = Terraria.GameContent.TextureAssets.Item[ModContent.ItemType<OldMiningHat>()].Value;
             if (CurrentAnimation == Animation.Hiding || CurrentAnimation == Animation.UnHiding)
             {
                 Rotation += 0.1f;
-                Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition - new Vector2(0, Projectile.height / 2) * (Projectile.frame + 0.1f * Projectile.frameCounter), texture.Bounds, lightColor, Rotation, texture.Size() / 2, Projectile.scale, Microsoft.Xna.Framework.Graphics.SpriteEffects.None);
+                Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition - new Vector2(0, Projectile.height / 2) * (Projectile.frame + 0.1f * Projectile.frameCounter), texture.Bounds, lightColor, Rotation, texture.Size() / 2, Projectile.scale, SpriteEffects.None);
             }
         }
     }
